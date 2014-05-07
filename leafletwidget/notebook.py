4a8424deb952c47c44f525db1222754210e09e1b
Brian E. Granger <ellisonbg@gmail.com>
2014-05-07 12:23:55 -0500

    Adding notebook.py.

---
+++ leafletwidget/notebook.py
@@ -0,0 +1,17 @@
+from __future__ import print_function
+
+import os
+
+from IPython.display import display, HTML, Javascript
+
+leaflet_css = '<link rel="stylesheet" href="http://cdn.leafletjs.com/leaflet-0.7.2/leaflet.css" />'
+leaftlet_js = "http://cdn.leafletjs.com/leaflet-0.7.2/leaflet"
+
+def get_static_path():
+    return os.path.join(os.path.split(__file__)[0], 'static')
+
+def initialize_notebook(leaflet_css=leaflet_css, leaflet_js=leaflet_css):
+    display(HTML(leaflet_css))
+    for filename in ['map.js', 'layer.js']:
+        with open(os.path.join(get_static_path(), 'js', filename)) as f:
+            display(Javascript(f.read()))

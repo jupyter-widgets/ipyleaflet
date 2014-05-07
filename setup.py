c64bcb0afe10ba72ac116e5c2ce293d6dbb2b877
Brian E. Granger <ellisonbg@gmail.com>
2014-05-07 11:39:47 -0500

    Initial files.

---
+++ setup.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name='leafletwidget',
+    version='0.1',
+    description='An IPython widget for dynamic Leaflet maps',
+    author='Brian E. Granger',
+    author_email='ellisonbg@gmail.com',
+    license='MIT License',
+    url='https://github.com/ellisonbg/leafletwidget',
+    keywords='data visualization',
+    classifiers=['Development Status :: 4 - Beta',
+                 'Programming Language :: Python :: 2.7',
+                 'License :: OSI Approved :: MIT License'],
+    packages=['leaflet'],
+    package_data={'': ['*.js',
+                       '*.css',
+                       'static/*.js',
+                       'static/*.css']}
+)

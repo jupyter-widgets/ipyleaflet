0fe4dbefbea195cc967b083b9fed88b36aaaa617
Brian E. Granger <ellisonbg@gmail.com>
2014-05-08 14:23:56 -0500

    Works on all parts of the widgets.

--- leafletwidget/leaflet.py
+++ leafletwidget/leaflet.py
@@ -12,9 +12,13 @@ def_loc = [32.3226932,-90.9019257]
 class LayerException(Exception):
     pass
 
+
 class Layer(widgets.Widget):
     _view_name = Unicode('LeafletLayerView', sync=True)
     bottom = Bool(False, sync=True)
+    options = List(trait=Unicode, allow_none=False, sync=True)
+    def _options_default(self):
+        return [name for name in self.traits(o=True)]
 
 
 class UILayer(Layer):
@@ -25,16 +29,16 @@ class Marker(UILayer):
     _view_name = Unicode('LeafletMarkerView', sync=True)
     # read/write
     location = List(def_loc, sync=True)
-    z_index_offset = Int(0, sync=True)
-    opacity = Float(1.0, sync=True)
+    z_index_offset = Int(0, sync=True, o=True)
+    opacity = Float(1.0, sync=True, o=True)
     # write
-    clickable = Bool(True, sync=True)
-    draggable = Bool(False, sync=True)
-    keyboard = Bool(True, sync=True)
-    title = Unicode('', sync=True)
-    alt = Unicode('', sync=True)
-    rise_on_hover = Bool(False, sync=True)
-    rise_offset = Int(250, sync=True)
+    clickable = Bool(True, sync=True, o=True)
+    draggable = Bool(False, sync=True, o=True)
+    keyboard = Bool(True, sync=True, o=True)
+    title = Unicode('', sync=True, o=True)
+    alt = Unicode('', sync=True, o=True)
+    rise_on_hover = Bool(False, sync=True, o=True)
+    rise_offset = Int(250, sync=True, o=True)
 
 
 class Popup(UILayer):
@@ -49,20 +53,20 @@ class TileLayer(RasterLayer):
     _view_name = Unicode('LeafletTileLayerView', sync=True)
     bottom = Bool(True, sync=True)
     url = Unicode('http://otile1.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.png', sync=True)
-    min_zoom = Int(0, sync=True)
-    max_zoom = Int(18, sync=True)
-    tile_size = Int(256, sync=True)
-    attribution = Unicode('Map data (c) <a href="http://openstreetmap.org">OpenStreetMap</a> contributors', sync=True)
-    opacity = Float(1.0, sync=True)
-    detect_retina = Bool(False, sync=True)
+    min_zoom = Int(0, sync=True, o=True)
+    max_zoom = Int(18, sync=True, o=True)
+    tile_size = Int(256, sync=True, o=True)
+    attribution = Unicode('Map data (c) <a href="http://openstreetmap.org">OpenStreetMap</a> contributors', sync=True, o=True)
+    opacity = Float(1.0, sync=True, o=True)
+    detect_retina = Bool(False, sync=True, o=True)
 
 
 class ImageOverlay(RasterLayer):
     _view_name = Unicode('LeafletImageOverlayView', sync=True)
     url = Unicode('', sync=True)
     bounds = List([def_loc, def_loc], sync=True, help="SW and NE corners of the image")
-    opacity = Float(1.0, sync=True)
-    attribution = Unicode('', sync=True)
+    opacity = Float(1.0, sync=True, o=True)
+    attribution = Unicode('', sync=True, o=True)
 
 
 class VectorLayer(Layer):
@@ -71,21 +75,26 @@ class VectorLayer(Layer):
 
 class Path(VectorLayer):
     _view_name = Unicode('LeafletPathView', sync=True)
-    stroke = Bool(True, sync=True)
-    color = Unicode('#03F', sync=True)
-    weight = Int(5, sync=True)
-    opacity = Float(0.5, sync=True)
-    fill = Bool(True, sync=True)
-    fill_color = Unicode('#03F', sync=True)
-    fill_opacity = Float(0.2, sync=True)
-    # More options that I havn't implemented go here
+    stroke = Bool(True, sync=True, o=True)
+    color = Unicode('#03F', sync=True, o=True)
+    weight = Int(5, sync=True, o=True)
+    opacity = Float(0.5, sync=True, o=True)
+    fill = Bool(True, sync=True, o=True)
+    fill_color = Unicode('#03F', sync=True, o=True)
+    fill_opacity = Float(0.2, sync=True, o=True)
+    dash_array = Unicode('', sync=True, o=True)
+    line_cap = Unicode('', sync=True, o=True)
+    line_join = Unicode('', sync=True, o=True)
+    clickable = Bool(True, sync=True, o=True)
+    pointer_events = Unicode('', sync=True, o=True)
+    class_name = Unicode('', sync=True, o=True)
 
 
 class Polyline(Path):
     _view_name = Unicode('LeafletPolylineView', sync=True)
     locations = List([], sync=True)
-    smooth_factor = Float(1.0, sync=True)
-    no_clip = Bool(False, sync=True)
+    smooth_factor = Float(1.0, sync=True, o=True)
+    no_clip = Bool(False, sync=True, o=True)
 
 
 class Polygon(Polyline):
@@ -94,6 +103,7 @@ class Polygon(Polyline):
 
 class Rectangle(Polygon):
     _view_name = Unicode('LeafletPolygonView', sync=True)
+    bounds = List([], sync=True, help="list of SW and NE location tuples")
 
 
 class Circle(Path):
@@ -135,17 +145,6 @@ class GeoJSON(FeatureGroup):
     _view_name = Unicode('LeafletGeoJSONView', sync=True)
     data = Dict({}, sync=True)
     style = Dict({}, sync=True)
-    
-    def _load_json(self, filename):
-        with open(filename) as f:
-            data = json.load(f)
-        return data
-    
-    def load_data(self, filename):
-        self.data = self._load_json(filename)
-
-    def load_style(self, filename):
-        self.style = self._load_json(filename)
 
 
 class MultiPolyline(FeatureGroup):
@@ -160,15 +159,47 @@ class Map(widgets.DOMWidget):
 
     _view_name = Unicode('LeafletMapView', sync=True)
     
-    location = List(def_loc, sync=True)
-    width = Unicode('600px', sync=True)
-    height = Unicode('400px', sync=True)
-    zoom_start = Int(12, sync=True)
-    zoom = Int(12, sync=True)
-    max_zoom = Int(18, sync=True)
-    min_zoom = Int(1, sync=True)
+    # Map options
+    center = List(def_loc, sync=True, o=True)
+    width = Unicode('600px', sync=True, o=True)
+    height = Unicode('400px', sync=True, o=True)
+    zoom_start = Int(12, sync=True, o=True)
+    zoom = Int(12, sync=True, o=True)
+    max_zoom = Int(18, sync=True, o=True)
+    min_zoom = Int(1, sync=True, o=True)
+    # Interaction options
+    dragging = Bool(True, sync=True, o=True)
+    touch_zoom = Bool(True, sync=True, o=True)
+    scroll_wheel_zoom = Bool(False, sync=True, o=True)
+    double_click_zoom = Bool(True, sync=True, o=True)
+    box_zoom = Bool(True, sync=True, o=True)
+    tap = Bool(True, sync=True, o=True)
+    tap_tolerance = Int(15, sync=True, o=True)
+    world_copy_jump = Bool(False, sync=True, o=True)
+    close_popup_on_click = Bool(True, sync=True, o=True)
+    bounce_at_zoom_limits = Bool(True, sync=True, o=True)
+    keyboard = Bool(True, sync=True, o=True)
+    keyboard_pan_offset = Int(80, sync=True, o=True)
+    keyboard_zoom_offset = Int(1, sync=True, o=True)
+    inertia = Bool(True, sync=True, o=True)
+    inertia_deceleration = Int(3000, sync=True, o=True)
+    inertia_max_speed = Int(1500, sync=True, o=True)
+    # inertia_threshold = Int(?, sync=True, o=True)
+    zoom_control = Bool(True, sync=True, o=True)
+    attribution_control = Bool(True, sync=True, o=True)
+    # fade_animation = Bool(?, sync=True, o=True)
+    # zoom_animation = Bool(?, sync=True, o=True)
+    zoom_animation_threshold = Int(4, sync=True, o=True)
+    # marker_zoom_animation = Bool(?, sync=True, o=True)
+
+    options = List(trait=Unicode, allow_none=False, sync=True)
+    def _options_default(self):
+        return [name for name in self.traits(o=True)]
+
     tiles_url = Unicode('http://otile1.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.png', sync=True)
     tiles_attr = Unicode('Map data (c) <a href="http://openstreetmap.org">OpenStreetMap</a> contributors', sync=True)
+
+
     _south = Float(def_loc[0], sync=True)
     _north = Float(def_loc[0], sync=True)
     _east = Float(def_loc[1], sync=True)
@@ -196,7 +227,11 @@ class Map(widgets.DOMWidget):
     @property
     def bounding_polygon(self):
         return [(self.north,self.west),(self.north,self.east),(self.south,self.east),(self.south,self.west)]
-    #     
+
+    @property
+    def bounds(self):
+        return [(self.south,self.west),(self.north,self.east)]
+
     def __init__(self, **kwargs):
         super(Map, self).__init__(**kwargs)
         self.on_displayed(self._fire_layers_displayed)
--- leafletwidget/static/leaflet.js
+++ leafletwidget/static/leaflet.js
@@ -4,17 +4,44 @@ require.config({paths: {leaflet: "http://cdn.leafletjs.com/leaflet-0.7.2/leaflet
 
 require(["widgets/js/widget", "leaflet"], function(WidgetManager, leaflet) {
 
+    function camel_case(input) {
+        // Convert from foo_bar to fooBar 
+        return input.toLowerCase().replace(/_(.)/g, function(match, group1) {
+            return group1.toUpperCase();
+        });
+    }
+    
     var LeafletLayerView = IPython.WidgetView.extend({
         
         initialize: function (parameters) {
             console.log('Layer.initialize');
             LeafletLayerView.__super__.initialize.apply(this, arguments);
-            this.map_view = this.options.map_view;
+            // Remove this line after testing...
+            this.model.on('displayed', this.test_display, this);
+            // this.map_view = this.options.map_view;
         },
-
+    
+        // Remove this method after testing...
+        test_display: function () {
+            console.log('displayed!!!');
+        },
+    
         render: function () {
+            console.log('options', this.get_options());
             this.create_obj();
-        }
+        },
+    
+        get_options: function () {
+            var o = this.model.get('options');
+            var options = {};
+            var key;
+            for (var i=0; i<o.length; i++) {
+                key = o[i];
+                // Convert from foo_bar to fooBar that Leaflet.js uses
+                options[camel_case(key)] = this.model.get(key);
+            }
+            return options;
+        },
     });
     WidgetManager.register_widget_view('LeafletLayerView', LeafletLayerView);
 
@@ -23,111 +50,162 @@ require(["widgets/js/widget", "leaflet"], function(WidgetManager, leaflet) {
     var LeafletUILayerView = LeafletLayerView.extend({
     });
     WidgetManager.register_widget_view('LeafletUILayerView', LeafletUILayerView);
-
-
+    
+    
     var LeafletMarkerView = LeafletUILayerView.extend({
+        
+        create_obj: function () {
+            this.obj = leaflet.marker(
+                this.model.get('location'),
+                this.get_options()
+            );
+        },
+        
     });
     WidgetManager.register_widget_view('LeafletMarkerView', LeafletMarkerView);
-
-
+    
+    
     var LeafletPopupView = LeafletUILayerView.extend({
     });
     WidgetManager.register_widget_view('LeafletPopupView', LeafletPopupView);
-
-
+    
+    
     // RasterLayer
     var LeafletRasterLayerView = LeafletLayerView.extend({
     });
     WidgetManager.register_widget_view('LeafletRasterLayerView', LeafletRasterLayerView);
-
-
+    
+    
     var LeafletTileLayerView = LeafletRasterLayerView.extend({
+        
+        create_obj: function () {
+            this.obj = leaflet.tileLayer(
+                this.model.get('url'),
+                this.get_options()
+            );
+        },
+        
     });
     WidgetManager.register_widget_view('LeafletTileLayerView', LeafletTileLayerView);
-
-
+    
+    
     var LeafletImageOverlayView = LeafletRasterLayerView.extend({
+        
+        create_obj: function () {
+            this.obj = leaflet.imageOverlay(
+                this.model.get('url'),
+                this.model.get('bounds'),
+                this.get_options()
+            );
+        },
+        
     });
     WidgetManager.register_widget_view('LeafletImageOverlayView', LeafletImageOverlayView);
-
-
+    
+    
     // VectorLayer
     var LeafletVectorLayerView = LeafletLayerView.extend({
     });
     WidgetManager.register_widget_view('LeafletVectorLayerView', LeafletVectorLayerView);
-
-
+    
+    
     var LeafletPathView = LeafletVectorLayerView.extend({
     });
     WidgetManager.register_widget_view('LeafletPathView', LeafletPathView);
-
-
+    
+    
     var LeafletPolylineView = LeafletPathView.extend({
+        
+        create_obj: function () {
+            this.obj = leaflet.polyline(
+                this.model.get('locations'),
+                this.get_options()
+            );
+        },
+        
     });
     WidgetManager.register_widget_view('LeafletPolylineView', LeafletPolylineView);
-
-
+    
+    
     var LeafletPolygonView = LeafletPolylineView.extend({
-
+    
         create_obj: function () {
             console.log('Polygon.create_obj', this.model.get('locations'));
-            this.obj = leaflet.polygon(this.model.get('locations'));
+            this.obj = leaflet.polygon(this.model.get('locations'), this.get_options());
+        },
+    
+    });
+    WidgetManager.register_widget_view('LeafletPolygonView', LeafletPolygonView);
+    
+    
+    var LeafletRectangleView = LeafletPolygonView.extend({
+        
+        create_obj: function () {
+            this.obj = leaflet.rectangle(
+                this.model.get('bounds'),
+                this.get_options()
+            );
         },
 
     });
-    WidgetManager.register_widget_view('LeafletPolygonView', LeafletPolygonView);
-
-
-    var LeafletRectangleView = LeafletPolygonView.extend({
-    });
     WidgetManager.register_widget_view('LeafletRectangleView', LeafletRectangleView);
-
-
+    
+    
     var LeafletCircleView = LeafletPathView.extend({
         
         create_obj: function () {
             console.log('Circle.create_obj', this.model.get('location'));
-            this.obj = leaflet.circle(this.model.get('location'), this.model.get('radius'));
+            this.obj = leaflet.circle(
+                this.model.get('location'), this.model.get('radius'),
+                this.get_options()
+            );
         },
-
+    
     });
     WidgetManager.register_widget_view('LeafletCircleView', LeafletCircleView);
-
-
+    
+    
     var LeafletCircleMarkerView = LeafletCircleView.extend({
-
+    
         create_obj: function () {
-            console.log('CircleMarker.create_obj', this.model.get('location'));
-            this.obj = leaflet.circleMarker(this.model.get('location'), this.model.get('radius'));
+            this.obj = leaflet.circleMarker(
+                this.model.get('location'), this.model.get('radius'),
+                this.get_options()
+            );
         },
-
+    
     });
     WidgetManager.register_widget_view('LeafletCircleMarkerView', LeafletCircleMarkerView);
-
-
+    
+    
     // LayerGroup
     var LeafletLayerGroupView = LeafletLayerView.extend({
     });
     WidgetManager.register_widget_view('LeafletLayerGroupView', LeafletLayerGroupView);
-
-
+    
+    
     var LeafletFeatureGroupView = LeafletLayerGroupView.extend({
     });
     WidgetManager.register_widget_view('LeafletFeatureGroupView', LeafletFeatureGroupView);
-
-
+    
+    
     var LeafletMultiPolylineView = LeafletFeatureGroupView.extend({
     });
     WidgetManager.register_widget_view('LeafletMultiPolylineView', LeafletMultiPolylineView);
-
-
+    
+    
     var LeafletGeoJSONView = LeafletFeatureGroupView.extend({
-
+    
         create_obj: function () {
-            console.log('CircleMarker.create_obj', this.model.get('location'));
-            this.obj = leaflet.geoJson(this.model.get('data'));
+            var style = this.model.get('style');
+            if ($.isEmptyObject(style)) {
+                style = function (feature) {
+                    console.log(feature.properties.style);
+                    return feature.properties.style;
+                } 
+            }
+            this.obj = leaflet.geoJson(this.model.get('data'), {style: style});
         },
-
     });
     WidgetManager.register_widget_view('LeafletGeoJSONView', LeafletGeoJSONView);
 
@@ -135,14 +213,14 @@ require(["widgets/js/widget", "leaflet"], function(WidgetManager, leaflet) {
     var LeafletMultiPolygonView = LeafletFeatureGroupView.extend({
     });
     WidgetManager.register_widget_view('LeafletMultiPolygonView', LeafletMultiPolygonView);
-
-
+    
+    
     var LeafletMapView = IPython.DOMWidgetView.extend({
         
         initialize: function (options) {
             LeafletMapView.__super__.initialize.apply(this, arguments);
         },
-
+    
         // Layer management
         update_layers: function (old_list, new_list) {
             this.do_diff(
@@ -152,65 +230,82 @@ require(["widgets/js/widget", "leaflet"], function(WidgetManager, leaflet) {
                 $.proxy(this.add_child_model, this)
             );
         },
-
+    
         remove_child_model: function (child_model) {
             var child_view = this.child_views[child_model.id];
-            this.map.removeLayer(child_view.obj);
+            this.obj.removeLayer(child_view.obj);
             this.delete_child_view(child_model);
         },
-
+    
         add_child_model: function (child_model) {
             var child_view = this.create_child_view(child_model);
-            console.log('Map.add_child_view', child_model);
-            this.map.addLayer(child_view.obj);
+            this.obj.addLayer(child_view.obj);
         },
-
+    
         // Rendering
         render: function () {
             this.$el.width(this.model.get('width')).height(this.model.get('height'));
             this.model.on('displayed', this.render_leaflet, this);
         },
-
+    
         render_leaflet: function () {
             var that = this;
-            this.map = leaflet.map(this.$el.get(0));
-            this.map.setView(this.model.get('location'), this.model.get('zoom_start'));
+            console.log('options', this.get_options());
+            this.create_obj();
+            // this.map = leaflet.map(this.$el.get(0));
+            // this.map.setView(this.model.get('location'), this.model.get('zoom_start'));
             leaflet.tileLayer(this.model.get('tiles_url'), {
                 attribution: this.model.get('tiles_attr'),
                 maxZoom: this.model.get('max_zoom'),
                 minZoom: this.model.get('min_zoom'),
-            }).addTo(this.map);
+            }).addTo(this.obj);
             this.update_layers([], this.model.get('layers'));
             this.leaflet_events();
             this.model_events();
             this.update_bounds();
         },
-
+    
+        create_obj: function () {
+            this.obj = leaflet.map(this.$el.get(0), this.get_options());
+        },
+    
+        get_options: function () {
+            var o = this.model.get('options');
+            var options = {};
+            var key;
+            for (var i=0; i<o.length; i++) {
+                key = o[i];
+                // Convert from foo_bar to fooBar that Leaflet.js uses
+                options[camel_case(key)] = this.model.get(key);
+            }
+            return options;
+        },
+    
         leaflet_events: function () {
             var that = this;
-            this.map.on('moveend', function (e) {
+            this.obj.on('moveend', function (e) {
                 var c = e.target.getCenter();
-                that.model.set('location', [c.lat, c.lng]);
+                that.model.set('center', [c.lat, c.lng]);
                 that.touch();
                 that.update_bounds();
             });
-            this.map.on('zoomend', function (e) {
+            this.obj.on('zoomend', function (e) {
                 var z = e.target.getZoom();
                 that.model.set('zoom', z);
                 that.touch();
                 that.update_bounds();
             });
         },
-
+    
         update_bounds: function () {
-            var b = this.map.getBounds();
+            var b = this.obj.getBounds();
             this.model.set('_north', b.getNorth());
             this.model.set('_south', b.getSouth());
             this.model.set('_east', b.getEast());
             this.model.set('_west', b.getWest());
             this.touch();
         },
-
+    
         model_events: function () {
             var that = this;
             this.model.on('msg:custom', this.handle_msg, this);
@@ -218,11 +313,11 @@ require(["widgets/js/widget", "leaflet"], function(WidgetManager, leaflet) {
                 that.update_layers(model.previous('layers'), value);
             });
             this.model.on('change:zoom', function () {
-                that.map.setZoom(that.model.get('zoom'));
+                that.obj.setZoom(that.model.get('zoom'));
                 that.update_bounds();
             });
-            this.model.on('change:location', function () {
-                that.map.panTo(that.model.get('location'));
+            this.model.on('change:center', function () {
+                that.obj.panTo(that.model.get('center'));
                 that.update_bounds();
             });
         },
@@ -233,7 +328,7 @@ require(["widgets/js/widget", "leaflet"], function(WidgetManager, leaflet) {
                     break;
             }
         },
-
+    
     });
     WidgetManager.register_widget_view('LeafletMapView', LeafletMapView);
 

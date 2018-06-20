ce4042fe220c41695447b4e1a473debe1afb33aa
Sylvain Corlay <sylvain.corlay@gmail.com>
2018-06-21 01:15:22 +0200

    Release 0.8.4

--- ipyleaflet/__init__.py
+++ ipyleaflet/__init__.py
@@ -1,6 +1,12 @@
 from ._version import version_info, __version__
 
-from .leaflet import *
+# Allow dependencies to ipyleaflet to not be installed upon post-link for
+# conda-build.
+
+try:
+    from .leaflet import *
+except ImportError:
+    pass
 
 def _jupyter_nbextension_paths():
     return [{

d01c82567ec219890b90705e7237ca0f237bfabf
martinRenou <martin.renou@gmail.com>
2018-07-03 15:39:08 +0200

    Rename velocity attributes

--- ipyleaflet/xarray_ds.py
+++ ipyleaflet/xarray_ds.py
@@ -1,10 +1,10 @@
 def ds_x_to_json(ds, widget):
     return ds2json(
             ds,
-            widget.u_var,
-            widget.v_var,
-            widget.lat_dim,
-            widget.lon_dim,
+            widget.zonal_speed,
+            widget.meridional_speed,
+            widget.latitude_dimension,
+            widget.longitude_dimension,
             widget.units
             )
 

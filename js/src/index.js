87e969f204235e916a7453aef5f9ac0f44fc5b2d
martinRenou <martin.renou@gmail.com>
2019-06-13 11:09:02 +0200

    Add jlab themes support

--- js/src/index.js
+++ js/src/index.js
@@ -1,17 +1,3 @@
-// Load css
-require('leaflet/dist/leaflet.css');
-require('leaflet-draw/dist/leaflet.draw.css');
-require('leaflet.markercluster/dist/MarkerCluster.css');
-require('leaflet.markercluster/dist/MarkerCluster.Default.css');
-require('leaflet-measure/dist/leaflet-measure.css');
-require('leaflet-fullscreen/dist/leaflet.fullscreen.css');
-require('./jupyter-leaflet.css');
-
-// Forcibly load the marker icon images to be in the bundle.
-require('leaflet/dist/images/marker-shadow.png');
-require('leaflet/dist/images/marker-icon.png');
-require('leaflet/dist/images/marker-icon-2x.png');
-
 // Export everything from jupyter-leaflet and the npm package version number.
 var _oldL = window.L;
 module.exports = require('./jupyter-leaflet.js');
@@ -22,4 +8,3 @@ if (_oldL !== undefined && _oldL !== window.L) {
     console.log("Existing `L` detected, running ipyleaflet's Leaflet in no-conflict mode as `ipyL`");
     ipyL = L.noConflict();
 }
-

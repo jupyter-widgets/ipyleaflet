5c8348f5b206c2301a82119e51a0f0f1c2f32c37
Kevin Giacobbi <kevin@richelieu.hygeos.com>
2019-10-28 11:48:53 +0100

    Add selected attributes to LayersControl

--- ipyleaflet/leaflet.py
+++ ipyleaflet/leaflet.py
@@ -915,6 +915,9 @@ class LayersControl(Control):
     _view_name = Unicode('LeafletLayersControlView').tag(sync=True)
     _model_name = Unicode('LeafletLayersControlModel').tag(sync=True)
 
+    selected_base = Unicode('').tag(sync=True)
+    selected_overlays = Tuple(trait=Unicode).tag(sync=True)
+
 
 class MeasureControl(Control):
     _view_name = Unicode('LeafletMeasureControlView').tag(sync=True)
--- js/src/controls/LayersControl.js
+++ js/src/controls/LayersControl.js
@@ -9,7 +9,9 @@ export class LeafletLayersControlModel extends control.LeafletControlModel {
     return {
       ...super.defaults(),
       _view_name: 'LeafletLayersControlView',
-      _model_name: 'LeafletLayersControlModel'
+      _model_name: 'LeafletLayersControlModel',
+      selected_base: '',
+      selected_overlays: []
     };
   }
 }
@@ -26,8 +28,42 @@ export class LeafletLayersControlView extends control.LeafletControlView {
 
   initialize(parameters) {
     super.initialize(parameters);
-    super.initialize(parameters);
     this.map_view = this.options.map_view;
+
+    this.map_view.obj.on("overlayadd", (event) => {
+        var sel = this.model.get('selected_overlays');
+        var pos = sel.indexOf(event.name);
+
+        if(pos == -1) {
+            // Make a copy of the list so that backbone sees the change
+            sel = [...sel];
+            sel.push(event.name);
+
+            this.model.set('selected_overlays', sel);
+            this.touch();
+        }
+    });
+
+    this.map_view.obj.on("overlayremove", (event) => {
+        var sel = this.model.get('selected_overlays');
+        var pos = sel.indexOf(event.name);
+
+        if(pos != -1) {
+            // Make a copy of the list so that backbone sees the change
+            sel = [...sel];
+            sel.splice(pos, 1);
+
+            this.model.set('selected_overlays', sel);
+            this.touch();
+        }
+    });
+
+    this.map_view.obj.on("baselayerchange", (event) => {
+        this.model.set('selected_base', event.name);
+        this.touch();
+    });
+
+    this.initializeSelectedAttributes();
   }
 
   toggle_obj() {
@@ -51,17 +87,46 @@ export class LeafletLayersControlView extends control.LeafletControlView {
           }
           return ov;
         }, {});
+        var selection = [...this.model.get('selected_overlays')];
         var overlays = views.reduce(function(ov, view) {
           if (!view.model.get('base')) {
-            ov[view.model.get('name')] = view.obj;
+            var name = view.model.get("name");
+            ov[name] = view.obj;
+            if (!selection.includes(name)) {
+              selection.push(name);
+            }
           }
           return ov;
         }, {});
+        this.model.set('selected_overlays', selection);
+        this.touch();
         this.obj = L.control.layers(baselayers, overlays, this.get_options());
         return this;
       })
       .then(() => {
         this.obj.addTo(this.map_view.obj);
       });
+  }
+
+  initializeSelectedAttributes() {
+      var layers = this.map_view.model.get('layers');
+
+      var baselayerNames = layers.reduce((baseLayerNames, layer) => {
+          if (layer.get("base")) {
+              baseLayerNames.push(layer.get('name'));
+          }
+          return baseLayerNames;
+      }, []);
+
+      var overlayNames = layers.reduce((overlayNames, layer) => {
+          if (!layer.get("base")) {
+              overlayNames.push(layer.get('name'));
+          }
+          return overlayNames;
+      }, []);
+
+      this.model.set('selected_base', baselayerNames[baselayerNames.length - 1]);
+      this.model.set('selected_overlays', overlayNames);
+      this.touch();
   }
 }

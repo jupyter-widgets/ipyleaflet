7642eabf3057bf2e6a28468d9a04386fab7fece1
David Brochart <david.brochart@gmail.com>
2019-11-20 09:44:37 +0100

    Add spinner when tiles are loading

--- js/src/jupyter-leaflet.js
+++ js/src/jupyter-leaflet.js
@@ -44,6 +44,7 @@ require('leaflet.markercluster/dist/MarkerCluster.css');
 require('leaflet.markercluster/dist/MarkerCluster.Default.css');
 require('leaflet-measure/dist/leaflet-measure.css');
 require('leaflet-fullscreen/dist/leaflet.fullscreen.css');
+require('spin.js/spin.css');
 require('./jupyter-leaflet.css');
 
 // Forcibly load the marker icon images to be in the bundle.

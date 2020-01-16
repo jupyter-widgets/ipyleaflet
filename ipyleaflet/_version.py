e30014e7110a5d028227c13e208a8696cfbb79dd
Sylvain Corlay <sylvain.corlay@gmail.com>
2020-01-16 13:01:46 +0100

    Release 0.12.0

--- ipyleaflet/_version.py
+++ ipyleaflet/_version.py
@@ -2,11 +2,11 @@
 # Distributed under the terms of the Modified BSD License.
 #
 
-version_info = (0, 11, 7, 'final', 0)
+version_info = (0, 12, 0, 'final', 0)
 
 _specifier_ = {'alpha': 'a', 'beta': 'b', 'candidate': 'rc', 'final': ''}
 
 __version__ = '%s.%s.%s%s'%(version_info[0], version_info[1], version_info[2],
   '' if version_info[3]=='final' else _specifier_[version_info[3]] + str(version_info[4]))
 
-EXTENSION_VERSION = '^0.11.7'
+EXTENSION_VERSION = '^0.12.0'

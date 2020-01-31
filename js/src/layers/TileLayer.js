6ae9a64ce8c386cb5203c530774bdf396545ecfb
martinRenou <martin.renou@gmail.com>
2020-01-31 11:26:25 +0100

    Monkey patch the GridLayer adding a refresh method
    Signed-off-by: martinRenou <martin.renou@gmail.com>

--- js/src/layers/TileLayer.js
+++ js/src/layers/TileLayer.js
@@ -59,7 +59,8 @@ export class LeafletTileLayerView extends rasterlayer.LeafletRasterLayerView {
       this.model,
       'change:url',
       function() {
-        this.obj.setUrl(this.model.get('url'));
+        this.obj.setUrl(this.model.get('url'), true);
+        this.obj.refresh();
       },
       this
     );

73610508c5d8da3923d764a5d61e3c02f1cea190
Yudai NAKATA <yudai.nkt@gmail.com>
2020-02-22 01:35:14 +0900

    Sync Path's fill_color with color
    This is compliant with the original Leaflet.

--- js/src/layers/Path.js
+++ js/src/layers/Path.js
@@ -13,7 +13,7 @@ export class LeafletPathModel extends vectorlayer.LeafletVectorLayerModel {
       color: '#0033FF',
       weight: 5,
       fill: true,
-      fill_color: '#0033FF',
+      fill_color: null,
       fill_opacity: 0.2,
       dash_array: null,
       line_cap: 'round',

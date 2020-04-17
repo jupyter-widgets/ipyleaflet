562622314f5118f06c5b0acbb994a18bf71b9cc2
martinRenou <martin.renou@gmail.com>
2020-04-17 16:21:29 +0200

    Release 0.12.4
    Signed-off-by: martinRenou <martin.renou@gmail.com>

--- ipyleaflet/_version.py
+++ ipyleaflet/_version.py
@@ -2,8 +2,8 @@
 # Distributed under the terms of the Modified BSD License.
 #
 
-version_info = (0, 12, 3)
+version_info = (0, 12, 4)
 
 __version__ = '%s.%s.%s' % (version_info[0], version_info[1], version_info[2])
 
-EXTENSION_VERSION = '^0.12.3'
+EXTENSION_VERSION = '^0.12.4'

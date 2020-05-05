83df3d673aec876dd807c47a8630476fcce4909e
stephaniegott <stephanie.gott@descarteslabs.com>
2020-05-05 15:44:45 -0600

    import xarray in Velocity, remove dependecy

--- ipyleaflet/leaflet.py
+++ ipyleaflet/leaflet.py
@@ -329,6 +329,15 @@ class VideoOverlay(RasterLayer):
 
 
 class Velocity(Layer):
+    try:
+        import xarray
+    except ModuleNotFoundError:
+        raise ImportError(
+            'The `xarray` module (required for use of the Velocity layer) is'
+            'not installed. Run "$ pip install xarray" to install the latest'
+            'version.'
+        )
+
     _view_name = Unicode('LeafletVelocityView').tag(sync=True)
     _model_name = Unicode('LeafletVelocityModel').tag(sync=True)
 

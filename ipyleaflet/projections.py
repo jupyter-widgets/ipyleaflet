fcd4f2587a5299160eb8fb9c718f06895635b0d5
David Brochart <david.brochart@gmail.com>
2020-06-16 08:17:00 +0200

    Remove comments

--- ipyleaflet/projections.py
+++ ipyleaflet/projections.py
@@ -30,7 +30,6 @@ projections = Bunch(
         custom=True,
         proj4def="""+proj=stere +lat_0=90 +lat_ts=70 +lon_0=-45 +k=1 +x_0=0 +y_0=0
                  +ellps=WGS84 +datum=WGS84 +units=m +no_defs""",
-        # origin and bounds for 250m resolution
         origin=[-4194304, 4194304],
         resolutions=[
             16384.0,
@@ -51,7 +50,6 @@ projections = Bunch(
         custom=True,
         proj4def="""+proj=stere +lat_0=-90 +lat_ts=-71 +lon_0=0 +k=1 +x_0=0 +y_0=0
                  +ellps=WGS84 +datum=WGS84 +units=m +no_defs""",
-        # origin and bounds for 250m resolution
         origin=[-4194304, 4194304],
         resolutions=[
             16384.0,

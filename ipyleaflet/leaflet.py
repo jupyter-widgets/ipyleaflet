2e9875ede72cfd70afff41884f42bb25c62971b3
Omkar Mestry <30625612+omanges@users.noreply.github.com>
2020-10-17 15:02:35 +0530

    Added support for DivIcon (#727)

--- ipyleaflet/leaflet.py
+++ ipyleaflet/leaflet.py
@@ -284,6 +284,53 @@ class Icon(UILayer):
         return value
 
 
+class DivIcon(UILayer):
+    """DivIcon class.
+
+    Custom lightweight icon for markers that uses a simple <div> element
+    instead of an image used for markers.
+
+    Attributes
+    ----------
+    html : string
+        Custom HTML code to put inside the div element,
+        empty by default.
+    bg_pos : tuple, default [0, 0]
+        Optional relative position of the background, in pixels.
+    icon_size: tuple, default None
+        The size of the icon, in pixels.
+    icon_anchor: tuple, default None
+        The coordinates of the "tip" of the icon (relative to its top left corner).
+        The icon will be aligned so that this point is at the marker's geographical
+        location. Centered by default if icon_size is specified.
+    popup_anchor: tuple, default None
+        The coordinates of the point from which popups will "open", relative to the
+        icon anchor.
+    """
+
+    _view_name = Unicode('LeafletDivIconView').tag(sync=True)
+    _model_name = Unicode('LeafletDivIconModel').tag(sync=True)
+
+    html = Unicode('').tag(sync=True, o=True)
+    bg_pos = List([0, 0], allow_none=True).tag(sync=True, o=True)
+    icon_size = List(default_value=None, allow_none=True).tag(sync=True, o=True)
+    icon_anchor = List(default_value=None, allow_none=True).tag(sync=True, o=True)
+    popup_anchor = List([0, 0], allow_none=True).tag(sync=True, o=True)
+
+    @validate('icon_size', 'icon_anchor', 'popup_anchor')
+    def _validate_attr(self, proposal):
+        value = proposal['value']
+
+        # Workaround Traitlets which does not respect the None default value
+        if value is None or len(value) == 0:
+            return None
+
+        if len(value) != 2:
+            raise TraitError('The value should be of size 2, but {} was given'.format(value))
+
+        return value
+
+
 class AwesomeIcon(UILayer):
     """AwesomeIcon class.
 
@@ -352,8 +399,7 @@ class Marker(UILayer):
     location = List(def_loc).tag(sync=True)
     opacity = Float(1.0, min=0.0, max=1.0).tag(sync=True)
     visible = Bool(True).tag(sync=True)
-    icon = Union((Instance(Icon), Instance(AwesomeIcon)), allow_none=True, default_value=None).tag(sync=True,
-                                                                                                   **widget_serialization)
+    icon = Union((Instance(Icon), Instance(AwesomeIcon), Instance(DivIcon)), allow_none=True, default_value=None).tag(sync=True, **widget_serialization)
 
     # Options
     z_index_offset = Int(0).tag(sync=True, o=True)

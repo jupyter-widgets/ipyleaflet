eb87d65376cbd9f15e2230aaea8d5b928f5f6341
Mátyás M Rada <rada.matyas@gmail.com>
2021-06-11 15:08:13 -0600

    Update leaflet.py

--- ipyleaflet/leaflet.py
+++ ipyleaflet/leaflet.py
@@ -1022,10 +1022,10 @@ class MarkerCluster(Layer):
     _model_name = Unicode('LeafletMarkerClusterModel').tag(sync=True)
 
     markers = Tuple().tag(trait=Instance(Marker), sync=True, **widget_serialization)
-    
     # Options
     disable_clustering_at_zoom = Int(18).tag(sync=True, o=True)
-
+    
+    
 class LayerGroup(Layer):
     """LayerGroup class.
 
@@ -2280,4 +2280,3 @@ class Map(DOMWidget, InteractMixin):
                     self.zoom -= 1
                     await wait_for_change(self, 'bounds')
                     break
-

cfb96ef32bbab33d0150e51e0275e0046d76289f
Mátyás M Rada <rada.matyas@gmail.com>
2021-06-14 00:54:24 -0600

    Added disable_marker_clustering_at_zoom option (#824)
    * Added disable_marker_clustering_at_zoom option
    
    * Update MarkerCluster.js
    
    * Update leaflet.py
    
    * Update leaflet.py

--- ipyleaflet/leaflet.py
+++ ipyleaflet/leaflet.py
@@ -1022,6 +1022,8 @@ class MarkerCluster(Layer):
     _model_name = Unicode('LeafletMarkerClusterModel').tag(sync=True)
 
     markers = Tuple().tag(trait=Instance(Marker), sync=True, **widget_serialization)
+    # Options
+    disable_clustering_at_zoom = Int(18).tag(sync=True, o=True)
 
 
 class LayerGroup(Layer):

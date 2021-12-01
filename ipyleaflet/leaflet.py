78c5455aa171ecac50193a9e74f793d4b51362a4
martinRenou <martin.renou@gmail.com>
2021-12-01 12:11:09 +0100

    Add location_found event to SearchControl

--- ipyleaflet/leaflet.py
+++ ipyleaflet/leaflet.py
@@ -1894,6 +1894,7 @@ class SearchControl(Control):
     marker = Instance(Marker, allow_none=True, default_value=None).tag(sync=True, **widget_serialization)
     layer = Instance(LayerGroup, allow_none=True, default_value=None).tag(sync=True, **widget_serialization)
 
+    _location_found_callbacks = Instance(CallbackDispatcher, ())
     _feature_found_callbacks = Instance(CallbackDispatcher, ())
 
     def __init__(self, **kwargs):
@@ -1901,8 +1902,11 @@ class SearchControl(Control):
         self.on_msg(self._handle_leaflet_event)
 
     def _handle_leaflet_event(self, _, content, buffers):
-        if content.get('event', '') == 'found':
-            self._feature_found_callbacks(**content)
+        if content.get('event', '') == 'locationfound':
+            if content.get('feature', None) is not None:
+                self._feature_found_callbacks(**content)
+            else:
+                self._location_found_callbacks(**content)
 
     def on_feature_found(self, callback, remove=False):
         """Add a found feature event listener for searching in GeoJSON layer.
@@ -1915,6 +1919,18 @@ class SearchControl(Control):
             Whether to remove this callback or not. Defaults to False.
         """
         self._feature_found_callbacks.register_callback(callback, remove=remove)
+
+    def on_location_found(self, callback, remove=False):
+        """Add a found location event listener. The callback will be called when a search result has been found.
+
+        Parameters
+        ----------
+        callback : callable
+            Callback function that will be called on location found event.
+        remove: boolean
+            Whether to remove this callback or not. Defaults to False.
+        """
+        self._location_found_callbacks.register_callback(callback, remove=remove)
 
 
 class MapStyle(Style, Widget):

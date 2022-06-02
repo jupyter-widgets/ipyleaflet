bdea8d41bbc1bc090c0a9e68bb36d055ed1e8f75
Jason Grout <jason@jasongrout.org>
2022-06-02 03:30:35 +0000

    Update webpack public path to use the default 'auto' option.
    This means the files should be able to be hosted on any CDN.
    
    Also update the webpack image loaders to use the new asset/resource type.
    
    See also https://github.com/jupyter-widgets/ipywidgets/pull/3464 and https://github.com/jupyter-widgets/widget-cookiecutter/pull/103

--- js/src/notebook.js
+++ js/src/notebook.js
@@ -1,7 +1,4 @@
 // Copyright (c) Jupyter Development Team.
 // Distributed under the terms of the Modified BSD License.
 
-// Setup notebook base URL
-__webpack_public_path__ = document.querySelector('body').getAttribute('data-base-url') + 'nbextensions/jupyter-leaflet/';
-
 module.exports = require('./index.js');
--- js/webpack.config.js
+++ js/webpack.config.js
@@ -1,76 +1,63 @@
-var path = require('path');
-var version = require('./package.json').version;
-var crypto = require('crypto');
+var path = require("path");
+var crypto = require("crypto");
 
 // Workaround for loaders using "md4" by default, which is not supported in FIPS-compliant OpenSSL
 var cryptoOrigCreateHash = crypto.createHash;
 crypto.createHash = (algorithm) =>
-  cryptoOrigCreateHash(algorithm == 'md4' ? 'sha256' : algorithm);
+  cryptoOrigCreateHash(algorithm == "md4" ? "sha256" : algorithm);
 
 var rules = [
-    {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-    },
-    {
-        test: /\.(jpg|png|gif|svg)$/,
-        use: [
-            {
-                loader: 'file-loader',
-                options: {
-                    esModule: false,
-                }
-            }
-        ],
-    }
+  { test: /\.css$/, use: ["style-loader", "css-loader"] },
+  { test: /\.(jpg|png|gif|svg)$/i, type: "asset/resource" },
 ];
 
 var resolve = {
-    fallback: {
-        "crypto": require.resolve("crypto-browserify"),
-        "buffer": require.resolve("buffer/"),
-        "stream": require.resolve("stream-browserify")
-    }
+  fallback: {
+    crypto: require.resolve("crypto-browserify"),
+    buffer: require.resolve("buffer/"),
+    stream: require.resolve("stream-browserify"),
+  },
 };
 
 module.exports = [
-    {// Notebook extension
-        entry: './src/extension.js',
-        output: {
-            filename: 'extension.js',
-            path: path.resolve(__dirname, '..', 'ipyleaflet', 'nbextension'),
-            libraryTarget: 'amd'
-        },
-        resolve: resolve
+  {
+    // Notebook extension
+    entry: "./src/extension.js",
+    output: {
+      filename: "extension.js",
+      path: path.resolve(__dirname, "..", "ipyleaflet", "nbextension"),
+      libraryTarget: "amd",
     },
-    {// jupyter-leaflet bundle for the classic notebook
-        entry: './src/notebook.js',
-        output: {
-            filename: 'index.js',
-            path: path.resolve(__dirname, '..', 'ipyleaflet', 'nbextension'),
-            libraryTarget: 'amd',
-            publicPath: '',
-        },
-        devtool: 'source-map',
-        module: {
-            rules: rules
-        },
-        externals: ['@jupyter-widgets/base'],
-        resolve: resolve
+    resolve: resolve,
+  },
+  {
+    // jupyter-leaflet bundle for the classic notebook
+    entry: "./src/notebook.js",
+    output: {
+      filename: "index.js",
+      path: path.resolve(__dirname, "..", "ipyleaflet", "nbextension"),
+      libraryTarget: "amd",
     },
-    {// jupyter-leaflet bundle for unpkg
-        entry: './src/embed.js',
-        output: {
-            filename: 'index.js',
-            path: path.resolve(__dirname, 'dist'),
-            libraryTarget: 'amd',
-            publicPath: 'https://unpkg.com/jupyter-leaflet@' + version + '/dist/'
-        },
-        devtool: 'source-map',
-        module: {
-            rules: rules
-        },
-        externals: ['@jupyter-widgets/base'],
-        resolve: resolve
-    }
+    devtool: "source-map",
+    module: {
+      rules: rules,
+    },
+    externals: ["@jupyter-widgets/base"],
+    resolve: resolve,
+  },
+  {
+    // jupyter-leaflet bundle for CDN
+    entry: "./src/embed.js",
+    output: {
+      filename: "index.js",
+      path: path.resolve(__dirname, "dist"),
+      libraryTarget: "amd",
+    },
+    devtool: "source-map",
+    module: {
+      rules: rules,
+    },
+    externals: ["@jupyter-widgets/base"],
+    resolve: resolve,
+  },
 ];

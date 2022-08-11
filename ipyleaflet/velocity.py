0df1c37ea76e4c50fccd19f55395a86d6539ce4d
HaudinFlorence <99649086+HaudinFlorence@users.noreply.github.com>
2022-08-11 14:09:15 +0200

    Update examples/Velocity_with_colormap.ipynb
    Co-authored-by: David Brochart <david.brochart@gmail.com>

--- ipyleaflet/velocity.py
+++ ipyleaflet/velocity.py
@@ -68,11 +68,40 @@ class Velocity(Layer):
     max_velocity = Float(10).tag(sync=True, o=True)
     velocity_scale = Float(0.005).tag(sync=True, o=True)
     colormap = Any(linear.YlOrRd_04)
-    color_scale = List([]).tag(sync=True, o=True)
+    #color_scale = List([]).tag(sync=True, o=True)
+    colors = [
+        "rgb(36,104, 180)",
+        "rgb(60,157, 194)",
+        "rgb(128,205,193)",
+        "rgb(151,218,168)",
+        "rgb(198,231,181)",
+        "rgb(238,247,217)",
+        "rgb(255,238,159)",
+        "rgb(252,217,125)",
+        "rgb(255,182,100)",
+        "rgb(252,150,75)",
+        "rgb(250,112,52)",
+        "rgb(245,64,32)",
+        "rgb(237,45,28)",
+        "rgb(220,24,32)",
+        "rgb(180,0,35)"
+    ]
+    color_scale = List(colors).tag(sync=True, o=True)
 
     @default('color_scale')
     def _default_color_scale(self):
         self.color_scale = []
+
+        for color in self.colormap.colors:
+            rgb_tuple = tuple(int(x * 256) for x in color[:3])
+            rgb_str = f"rgb{rgb_tuple}"
+            self.color_scale.append(rgb_str)
+
+        return self.color_scale
+
+    @default('colormap')
+    def _default_colormap(self):
+
 
         for color in self.colormap.colors:
             rgb_tuple = tuple(int(x * 256) for x in color[:3])

73b052a77d8eb6a40a3871726412cc924c8bfd77
Florence Haudin <haudin.florence@gmail.com>
2022-08-19 18:33:41 +0200

    Simplify Geoportal.js.

--- ipyleaflet/geoportal.py
+++ ipyleaflet/geoportal.py
@@ -3,20 +3,42 @@
 #
 
 
-from traitlets import Unicode
+from traitlets import Unicode, List
 from .leaflet import TileLayer
 
 
 
 class GeoportalWMTS(TileLayer):
-    """Geoportal class, with TileLayer as parent class
+    """GeoportalWMTS class, with TileLayer as parent class
 
 
     Attributes
     ----------
+    layer_name: string, default "ORTHOIMAGERY.ORTHOPHOTOS"
+        name of the layer to show.
+    apiKey: string, default "essentiels"
+        key to access a given ressource, detailed correspondances are given here : https://geoservices.ign.fr/documentation/services/tableau_ressources
     """
 
-    _view_name = Unicode('LeafletGeoportalView').tag(sync=True)
-    _model_name = Unicode('LeafletGeoportalModel').tag(sync=True)
-    name = Unicode("ORTHOIMAGERY.ORTHOPHOTOS").tag(sync=True, o=True)
+    _view_name = Unicode('LeafletGeoportalWMTSView').tag(sync=True)
+    _model_name = Unicode('LeafletGeoportalWMTSModel').tag(sync=True)
+    layer_name = Unicode("ORTHOIMAGERY.ORTHOPHOTOS").tag(sync=True, o=True)
+    apiKey = Unicode("essentiels").tag(sync=True, o=True)
 
+
+class GeoportalWMS(TileLayer):
+    """GeoportalWMS class, with TileLayer as parent class
+
+
+    Attributes
+    ----------
+    layer_name: string, default "GEOGRAPHICALGRIDSYSTEMS.PLANIGNV2"
+        name of the layer to show.
+    apiKey: string, default "cartes"
+        key to access a given ressource, detailed correspondances are given here : https://geoservices.ign.fr/documentation/services/tableau_ressources
+    """
+
+    _view_name = Unicode('LeafletGeoportalWMSView').tag(sync=True)
+    _model_name = Unicode('LeafletGeoportalWMSModel').tag(sync=True)
+    name = Unicode("GEOGRAPHICALGRIDSYSTEMS.PLANIGNV2").tag(sync=True, o=True)
+    layers = Unicode("essentiels").tag(sync=True, o=True)
--- js/src/layers/Geoportal.js
+++ js/src/layers/Geoportal.js
@@ -2,50 +2,41 @@
 // Distributed under the terms of the Modified BSD License.
 
 const L = require('../leaflet.js');
-const layer = require('./Layer.js');
+const layer = require('./TileLayer.js');
 const utils = require('../utils');
 
-export class LeafletGeoportalModel extends layer.LeafletLayerModel {
+export class LeafletGeoportalWMTSModel extends layer.LeafletTileLayerModel {
   defaults() {
     return {
       ...super.defaults(),
-      _view_name: 'LeafletGeoportalView',
-      _model_name: 'LeafletGeoportalModel',
-      name : "ORTHOIMAGERY.ORTHOPHOTOS"
+      _view_name: 'LeafletGeoportalWMTSView',
+      _model_name: 'LeafletGeoportalWMTSModel',
+      layer_name : "ORTHOIMAGERY.ORTHOPHOTOS",
+      apiKey : "essentiels"
     };
   }
 }
 
-export class LeafletGeoportalView extends layer.LeafletTileLayerView {
+export class LeafletGeoportalWMTSView extends layer.LeafletTileLayerView {
   create_obj() {
-    var options = this.get_options();
-    options.data = this.model.get('data');
-    this.obj = L.geoportalLayer.WMTS(this.model.get('name'))
-
+    this.obj = L.geoportalLayer.WMTS({layer : this.model.get('layer_name'), apiKey : this.model.get('apiKey')})
   }
+}
 
-  model_events() {
-    super.model_events();
-    this.listenTo(
-      this.model,
-      'change:data',
-      function() {
-        const data = this.model.get('data');
-        this.obj.setData(data);
-      },
-      this
-    );
-    // Separate display_options from the options to perform a shallow copy.
-    var key = 'display_options';
-    this.listenTo(
-      this.model,
-      'change:' + key,
-      function() {
-        var options = {};
-        options[utils.camel_case(key)] = { ...this.model.get(key) };
-        L.setOptions(this.obj, options);
-      },
-      this
-    );
+export class LeafletGeoportalWMSModel extends layer.LeafletTileLayerModel {
+  defaults() {
+    return {
+      ...super.defaults(),
+      _view_name: 'LeafletGeoportalWMSView',
+      _model_name: 'LeafletGeoportalWMSModel',
+      layer_name : "GEOGRAPHICALGRIDSYSTEMS.PLANIGNV2",
+      apiKey : "essentiels"
+    };
+  }
+}
+
+export class LeafletGeoportalWMSView extends layer.LeafletTileLayerView {
+  create_obj() {
+    this.obj = L.geoportalLayer.WMS({layer : this.model.get('layer_name'), apiKey : this.model.get('apiKey')})
   }
 }

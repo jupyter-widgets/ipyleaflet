42ceffac3c09ffb6eea9a21c4bb67694c6050bb9
Rambaud Pierrick <12rambau@users.noreply.github.com>
2023-08-06 12:40:13 +0000

    refactor: use modern packaging by moving everything to pyproject.toml

--- setup.py
+++ setup.py
@@ -8,18 +8,13 @@ from jupyter_packaging import (
     install_npm,
     ensure_targets,
     combine_commands,
-    get_version,
-    skip_if_exists
+    skip_if_exists,
 )
 
 # the name of the package
-name = 'ipyleaflet'
 long_description = 'A Jupyter widget for dynamic Leaflet maps'
 
 here = os.path.dirname(os.path.abspath(__file__))
-
-# Get ipyleaflet version
-version = get_version(os.path.join(name, '_version.py'))
 
 js_dir = os.path.join(here, 'js')
 
@@ -46,39 +41,6 @@ if is_repo:
 else:
     cmdclass['jsdeps'] = skip_if_exists(jstargets, js_command)
 
-setup_args = dict(
-    name=name,
-    version=version,
-    description='A Jupyter widget for dynamic Leaflet maps',
-    long_description=long_description,
-    license='MIT License',
-    python_requires = ">=3.7",
-    include_package_data=True,
-    install_requires=[
-        'ipywidgets>=7.6.0,<9',
-        'traittypes>=0.2.1,<3',
-        'xyzservices>=2021.8.1',
-        'branca>=0.5.0'
-    ],
-    packages=find_packages(),
-    zip_safe=False,
-    cmdclass=cmdclass,
-    author='Project Jupyter',
-    author_email='jupyter@googlegroups.com',
-    url='https://github.com/jupyter-widgets/ipyleaflet',
-    keywords=['ipython', 'jupyter', 'widgets', 'graphics', 'GIS'],
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
-        'Intended Audience :: Science/Research',
-        'Topic :: Multimedia :: Graphics',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10'
-    ],
-)
+setup_args = dict(cmdclass=cmdclass)
 
 setup(**setup_args)

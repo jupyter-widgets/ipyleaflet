d222c25cf2c6d2664d9bace171ad34bb92372e7c
Rambaud Pierrick <12rambau@users.noreply.github.com>
2023-08-06 13:23:25 +0000

    refactor: start correct deprecation notices from jupyter_packaging

--- setup.py
+++ setup.py
@@ -1,27 +1,16 @@
-# -*- coding: utf-8 -*-
+from pathlib import Path
+from setuptools import setup
 
-import os
-from setuptools import setup, find_packages
+from jupyter_packaging import *
 
-from jupyter_packaging import (
-    create_cmdclass,
-    install_npm,
-    ensure_targets,
-    combine_commands,
-    skip_if_exists,
-)
+ROOT = Path(__file__).parent
 
-# the name of the package
-long_description = 'A Jupyter widget for dynamic Leaflet maps'
-
-here = os.path.dirname(os.path.abspath(__file__))
-
-js_dir = os.path.join(here, 'js')
+js_dir = ROOT / 'js'
 
 # Representative files that should exist after a successful build
 jstargets = [
-    os.path.join('ipyleaflet/nbextension', 'index.js'),
-    os.path.join('ipyleaflet/labextension', 'package.json'),
+    str(Path('ipyleaflet/nbextension')/'index.js'),
+    str(Path('ipyleaflet/labextension')/'package.json'),
 ]
 
 data_files_spec = [
@@ -32,15 +21,11 @@ data_files_spec = [
 
 cmdclass = create_cmdclass('jsdeps', data_files_spec=data_files_spec)
 js_command = combine_commands(
-    install_npm(js_dir, npm=["yarn"], build_cmd='build'), ensure_targets(jstargets),
+    install_npm(js_dir, npm=["yarn"], build_cmd='build'),
+    ensure_targets(jstargets),
 )
 
-is_repo = os.path.exists(os.path.join(here, '.git'))
-if is_repo:
-    cmdclass['jsdeps'] = js_command
-else:
-    cmdclass['jsdeps'] = skip_if_exists(jstargets, js_command)
+is_repo = (ROOT / '.git').exists()
+cmdclass['jsdeps'] = js_command if is_repo else skip_if_exists(jstargets, js_command)
 
-setup_args = dict(cmdclass=cmdclass)
-
-setup(**setup_args)
+setup(cmdclass=cmdclass)

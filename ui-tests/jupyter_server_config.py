6ddbcb0caa2e6f4487be8ca2bb71f9c8999a007f
martinRenou <martin.renou@gmail.com>
2023-12-06 15:56:19 +0100

    Update galata setup (copying from bqplot's ui-tests)

--- ui-tests/jupyter_server_config.py
+++ ui-tests/jupyter_server_config.py
@@ -1,7 +1,3 @@
-c.ServerApp.port = 8888
-c.ServerApp.token = ""
-c.ServerApp.password = ""
-c.ServerApp.disable_check_xsrf = True
-c.ServerApp.open_browser = False
-c.LabApp.open_browser = False
-c.LabApp.expose_app_in_browser = True
+from jupyterlab.galata import configure_jupyter_server
+
+configure_jupyter_server(c)  # noqa F821
--- ui-tests/playwright.config.js
+++ ui-tests/playwright.config.js
@@ -5,6 +5,11 @@ module.exports = {
   expect: {
     toMatchSnapshot: { threshold: 0.33 },
   },
+  webServer: {
+    command: 'jlpm start',
+    url: 'http://localhost:8888/lab',
+    reuseExistingServer: !process.env.CI
+  },
   preserveOutput: 'failures-only',
   retries: 0,
   timeout: 600000,

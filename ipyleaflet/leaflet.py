d3dd2be379937a16d7f666bdd03d85ece40a13c9
gjmooney <gjmooney@asu.edu>
2023-12-15 13:18:56 +0100

    Add collapsed option to LayerControl

--- ipyleaflet/leaflet.py
+++ ipyleaflet/leaflet.py
@@ -1758,10 +1758,16 @@ class LayersControl(Control):
     """LayersControl class, with Control as parent class.
 
     A control which allows hiding/showing different layers on the Map.
+    Attributes
+    ----------------------
+    collapsed: bool, default True
+        Set whether control should be open or closed by default
     """
 
     _view_name = Unicode('LeafletLayersControlView').tag(sync=True)
     _model_name = Unicode('LeafletLayersControlModel').tag(sync=True)
+
+    collapsed = Bool(True).tag(sync=True, o=True)
 
 
 class MeasureControl(Control):

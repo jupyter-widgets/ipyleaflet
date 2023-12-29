186716261e80e50f95f8c487eac21be4af237141
gjmooney <gjmooney@asu.edu>
2023-12-29 09:47:37 +0100

    Update Polyline

--- js/src/layers/Polyline.ts
+++ js/src/layers/Polyline.ts
@@ -1,10 +1,10 @@
 // Copyright (c) Jupyter Development Team.
 // Distributed under the terms of the Modified BSD License.
-//@ts-nocheck
+import { Handler, Polyline } from 'leaflet';
 import L from '../leaflet';
-import * as path from './Path';
+import { LeafletPathModel, LeafletPathView } from './Path';
 
-export class LeafletPolylineModel extends path.LeafletPathModel {
+export class LeafletPolylineModel extends LeafletPathModel {
   defaults() {
     return {
       ...super.defaults(),
@@ -17,53 +17,52 @@ export class LeafletPolylineModel extends path.LeafletPathModel {
   }
 }
 
-export class LeafletPolylineView extends path.LeafletPathView {
+interface ExtendedPolyline extends Polyline {
+  dragging?: Handler;
+  transform?: Transform;
+}
+
+interface Transform extends Handler {
+  reset: () => void;
+  setOptions: (options: {
+    scaling: string;
+    uniformScaling: string;
+    rotation: string;
+  }) => Handler;
+}
+
+export class LeafletPolylineView extends LeafletPathView {
+  obj: ExtendedPolyline;
+
   create_obj() {
     this.obj = L.polyline(this.model.get('locations'), this.get_options());
   }
 
   model_events() {
     super.model_events();
-    this.listenTo(
-      this.model,
-      'change:locations',
-      function () {
-        this.obj.setLatLngs(this.model.get('locations'));
-        if (this.obj.transform) {
-          this.obj.transform.reset();
-        }
-      },
-      this
-    );
+    this.listenTo(this.model, 'change:locations', () => {
+      this.obj.setLatLngs(this.model.get('locations'));
+      if (this.obj.transform) {
+        this.obj.transform.reset();
+      }
+    });
     this.model.on_some_change(
       ['scaling', 'uniform_scaling', 'rotation'],
       this.update_transform,
       this
     );
-    this.listenTo(
-      this.model,
-      'change:draggable',
-      () => {
-        if (this.obj.dragging) {
-          this.obj.dragging[
-            this.model.get('draggable') ? 'enable' : 'disable'
-          ]();
-        }
-      },
-      this
-    );
-    this.listenTo(
-      this.model,
-      'change:transform',
-      () => {
-        if (this.obj.transform) {
-          this.obj.transform[
-            this.model.get('transform') ? 'enable' : 'disable'
-          ]();
-        }
-      },
-      this
-    );
+    this.listenTo(this.model, 'change:draggable', () => {
+      if (this.obj.dragging) {
+        this.obj.dragging[this.model.get('draggable') ? 'enable' : 'disable']();
+      }
+    });
+    this.listenTo(this.model, 'change:transform', () => {
+      if (this.obj.transform) {
+        this.obj.transform[
+          this.model.get('transform') ? 'enable' : 'disable'
+        ]();
+      }
+    });
   }
 
   update_transform() {

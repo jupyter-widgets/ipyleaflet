c44a38b6d6a1ca7864093b0b88ca4eaf3ecf9e85
gjmooney <gjmooney@asu.edu>
2024-01-10 14:55:34 +0100

    Remove commented lines

--- ipyleaflet/_version.py
+++ ipyleaflet/_version.py
@@ -1,10 +1,5 @@
 # Copyright (c) Jupyter Development Team.
 # Distributed under the terms of the Modified BSD License.
-#
-
-# version_info = (0, 18, 1)
-
-# __version__ = "%s.%s.%s" % (version_info[0], version_info[1], version_info[2])
 
 EXTENSION_VERSION = "^0.18"
 __version__ = "0.18.1"

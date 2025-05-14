3f437437611623de59b4212e7b69e9962b51a1aa
Oliver Lopez <lopezv.oliver@gmail.com>
2025-05-14 14:00:37 +0300

    feat: add hover_style to GeomanDrawControl

--- python/ipyleaflet/ipyleaflet/leaflet.py
+++ python/ipyleaflet/ipyleaflet/leaflet.py
@@ -2320,6 +2320,9 @@ class GeomanDrawControl(DrawControlBase):
     polygon = Dict({ 'pathOptions': {} }).tag(sync=True)
     circlemarker = Dict({ 'pathOptions': {} }).tag(sync=True)
 
+    # Hover style (applies for all drawing modes)
+    hover_style = Dict().tag(sync=True) 
+
     # Disabled by default
     text = Dict().tag(sync=True)
 

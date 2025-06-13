eceb588baceb8e30b7d297fb5a81a1f915294d13
martinRenou <martin.renou@gmail.com>
2025-06-13 10:23:31 +0200

    Release 0.20.0

--- python/ipyleaflet/ipyleaflet/_version.py
+++ python/ipyleaflet/ipyleaflet/_version.py
@@ -1,5 +1,5 @@
 # Copyright (c) Jupyter Development Team.
 # Distributed under the terms of the Modified BSD License.
 
-EXTENSION_VERSION = "^0.19"
-__version__ = "0.19.2"
+EXTENSION_VERSION = "^0.20"
+__version__ = "0.20.0"
